c312ef8329fe1b1a3709be92495b821eb8125743
tbcarus <70680359+tbcarus@users.noreply.github.com>
2022-06-06 23:39:21 +0300

    HW03 basic

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -9,29 +12,62 @@ import java.util.List;
 
 @Repository
 public class JdbcMealRepository implements MealRepository {
+    private static final BeanPropertyRowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcMealRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (meal.isNew()) {
+            jdbcTemplate.update("INSERT INTO meals (user_id, date_time, description, calories) " +
+                            "VALUES (?, ?, ?, ?)",
+                    userId, meal.getDateTime(), meal.getDescription(), meal.getCalories());
+            return meal;
+        } else {
+            return jdbcTemplate.update(
+                    "UPDATE meals SET date_time = ?, description = ?, calories =? " +
+                            "WHERE user_id = ?",
+                    meal.getDateTime(), meal.getDescription(), meal.getCalories(), userId) != 0 ? meal : null;
+        }
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return jdbcTemplate.update("DELETE FROM meals WHERE id = ? AND user_id = ?", id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        List<Meal> meals = jdbcTemplate.query(
+                "SELECT id, date_time, description, calories " +
+                        "FROM meals " +
+                        "WHERE id = ? " +
+                        "AND user_id = ?", ROW_MAPPER, id, userId);
+        return DataAccessUtils.singleResult(meals);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return jdbcTemplate.query(
+                "SELECT id, date_time, description, calories " +
+                        "FROM meals " +
+                        "WHERE user_id = ? " +
+                        "ORDER BY date_time DESC", ROW_MAPPER, userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return jdbcTemplate.query(
+                "SELECT id, date_time, description, calories " +
+                        "FROM meals " +
+                        "WHERE user_id = ? " +
+                        "AND date_time >= ? " +
+                        "AND date_time < ? " +
+                        "ORDER BY date_time DESC", ROW_MAPPER, userId, startDateTime, endDateTime);
     }
 }
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -6,6 +7,17 @@ INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
        ('Admin', 'admin@gmail.com', 'admin'),
        ('Guest', 'guest@gmail.com', 'guest');
+
+INSERT INTO meals (user_id, date_time, description, calories)
+values (100000, '2020-01-30 10:00:00', 'Завтрак', 500),
+       (100000, '2020-01-30 13:00:00', 'Обед', 1000),
+       (100000, '2020-01-30 20:00:00', 'Ужин', 500),
+       (100000, '2020-01-31 00:00:00', 'Еда на граничное условие', 100),
+       (100000, '2020-01-31 10:00:00', 'Завтрак', 1000),
+       (100000, '2020-01-31 13:00:00', 'Обед', 500),
+       (100000, '2020-01-31 20:00:00', 'Ужин', 410),
+       (100001, '2015-06-01 14:00:00', 'Админ ланч', 510),
+       (100001, '2015-06-01 21:00:00', 'Админ ужин', 1500);
 
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
